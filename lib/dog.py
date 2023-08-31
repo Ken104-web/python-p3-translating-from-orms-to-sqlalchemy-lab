f806026e5e75413fc3ae1b6877d3bdfc1b4253ed
KenCorp <kennethwarui35@gmail.com>
2023-08-31 22:20:09 +0300

    Test done

--- lib/dog.py
+++ lib/dog.py
@@ -10,20 +10,24 @@ def save(session, dog):
     session.commit()
     pass
 
-def get_all(session, Dog):
+def get_all(session):
     return session.query(Dog).all()
     
     
-
 def find_by_name(session, name):
+    return session.query(Dog).filter_by(name=name).first()
     
     pass
 
 def find_by_id(session, id):
+    return session.query(Dog).filter_by(id=id).first()
     pass
 
 def find_by_name_and_breed(session, name, breed):
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
     pass
 
 def update_breed(session, dog, breed):
+    dog.breed = breed
+    session.commit()
     pass
